13ac01aea46051d1325a7e51a439201a545d5e02
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-05-21 04:28:00 +0530

    rest api w mongoose part 2

--- conFusionServer/routes/DishRouter.js
+++ conFusionServer/routes/DishRouter.js
@@ -42,8 +42,6 @@ dishRouter.route('/')
           .catch((err)=>next(err));
      });
 
-
-
 dishRouter.route('/:dishID')
      .get((req,res,next)=>{
           Dishes.findById(req.params.dishID)
@@ -76,6 +74,152 @@ dishRouter.route('/:dishID')
                res.statusCode=200;
                res.setHeader('ContentType','application/json');
                res.json(dish);
+          },(err)=>next(err))
+          .catch((err)=>next(err));
+     })
+
+
+
+
+     dishRouter.route('/:dishID/comments')
+     .get((req,res,next)=>{
+          Dishes.findById(req.params.dishID)
+          .then((dish)=>{
+               if(dish != null){
+                    res.statusCode=200;
+                    res.setHeader('ContentType','application/json');
+                    res.json(dish.comments);
+               }else{
+                    err = new Error('Dish with ID:'+req.params.dishID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+          },(err)=>next(err))
+          .catch((err)=>next(err));
+     })
+     .post((req,res,next)=>{
+          Dishes.findById(req.params.dishID)
+          .then((dish)=>{
+               if(dish != null){
+                    dish.comments.push(req.body);
+                    dish.save()
+                    .then((dish)=>{
+                         res.statusCode=200;
+                         res.setHeader('ContentType','application/json');
+                         res.json(dish);
+                    },(err)=>next(err))            //////////////////////////////
+               }else{
+                    err = new Error('Dish with ID:'+req.params.dishID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+          },(err)=>next(err))
+          .catch((err)=>next(err));
+     })
+     .put((req,res,next)=>{
+          res.statusCode = 403;
+          res.end('PUT operation not supported on /dishes/'+req.params.dishID+'/comments!');
+     })
+     .delete((req,res,next)=>{
+          Dishes.findById(req.params.dishID)
+          .then((dish)=>{
+               if(dish != null){
+                    for(var i=(dish.comments.length -1);i>=0;i--){
+                         dish.comments.id(dish.comments[i].id).remove();
+                    }
+                    dish.save()
+                    .then((dish)=>{
+                         res.statusCode=200;
+                         res.setHeader('ContentType','application/json');
+                         res.json(dish);
+                    });
+               }else{
+                    err = new Error('Dish with ID:'+req.params.dishID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+          },(err)=>next(err))
+          .catch((err)=>next(err));
+     });
+
+
+
+dishRouter.route('/:dishID/comments/:commentID')
+     .get((req,res,next)=>{
+          Dishes.findById(req.params.dishID)
+          .then((dish)=>{
+               if(dish != null && dish.comments.id(req.params.commentID)!=null){
+                    res.statusCode=200;
+                    res.setHeader('ContentType','application/json');
+                    res.json(dish.comments.id(req.params.commentID));
+               }else if(dish == null){
+                    err = new Error('Dish with ID:'+req.params.dishID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+               else {
+                    err = new Error('Comment with ID:'+req.params.commentID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+          },(err)=>next(err))
+          .catch((err)=>next(err));
+     })
+     .post((req,res,next)=>{
+          res.statusCode = 403;
+          res.end('POST operation not supported on /dishes/'+req.params.dishID+'/comments/'+req.params.commentID+'!');
+     })
+     .put((req,res,next)=>{
+          Dishes.findById(req.params.dishID)
+          .then((dish)=>{
+               if(dish != null && dish.comments.id(req.params.commentID)!=null){
+                    if(req.body.rating){
+                         dish.comments.id(req.params.commentID).rating = req.body.rating;
+                    }
+                    if(req.body.comment){
+                         dish.comments.id(req.params.commentID).comment = req.body.comment;
+                    }
+                    dish.save()
+                    .then((dish)=>{
+                         res.statusCode=200;
+                         res.setHeader('ContentType','application/json');
+                         res.json(dish);
+                    },(err)=>next(err))  
+               }else if(dish == null){
+                    err = new Error('Dish with ID:'+req.params.dishID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+               else {
+                    err = new Error('Comment with ID:'+req.params.commentID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+          },(err)=>next(err))
+          .catch((err)=>next(err));
+     })
+
+     .delete((req,res,next)=>{
+          Dishes.findById(req.params.dishID)
+          .then((dish)=>{
+               if(dish != null && dish.comments.id(req.params.commentID)!=null){
+                    dish.comments.id(req.params.commentID).remove();
+                    dish.save()
+                    .then((dish)=>{
+                         res.statusCode=200;
+                         res.setHeader('ContentType','application/json');
+                         res.json(dish);
+                    });
+               }else if(dish == null){
+                    err = new Error('Dish with ID:'+req.params.dishID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
+               else {
+                    err = new Error('Comment with ID:'+req.params.commentID+' does not EXISTS!!');
+                    err.status = 404;
+                    return next(err);
+               }
           },(err)=>next(err))
           .catch((err)=>next(err));
      })
