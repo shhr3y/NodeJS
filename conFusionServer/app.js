ffa63b9bc2facbbd9dd0c666808749f243f6f459
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-05-23 17:57:19 +0530

    express sessions

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 
 
 var indexRouter = require('./routes/index');
@@ -32,33 +35,53 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized:false,
+  resave:false,
+  store: new FileStore(),
+}));
 
 function auth(req,res,next){
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
+  console.log(req.session);
+  
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
 
-  if(!authHeader){
-    var err = new Error('You are not Authenticated!!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    next(err);
-    return; 
-  }
-
-  var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-
-
-  var username = auth[0];
-  var password = auth[1];
-
-  if(username === 'admin' && password === 'password'){
-    next();
+    if(!authHeader){
+      var err = new Error('You are not Authenticated!!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+  
+  
+    var username = auth[0];
+    var password = auth[1];
+  
+    if(username === 'admin' && password === 'password'){
+      req.session.user = 'admin';
+      next();
+    }else{
+      var err = new Error('Authentication not Accepted!!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err); 
+    }
   }else{
-    var err = new Error('Authentication not Accepted!!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err); 
+    if(req.session.user==='admin'){
+      next();
+    }
+    else{
+      var err = new Error('Authentication not Accepted!!');
+      err.status = 401;
+      return next(err); 
+    }
   }
 
 }
